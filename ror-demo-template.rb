3654d3c75fe7d59df16453418a4463ccc53f5afb
matjemeisje <matje@taalmonsters.nl>
2016-01-31 18:44:24 +0100

    Update stylesheet

--- ror-demo-template.rb
+++ ror-demo-template.rb
@@ -1,6 +1,9 @@
 apply "https://raw.github.com/Taalmonsters/rails-composer/master/devise-gmail-composer.rb"
 
-copy_file "https://raw.github.com/Taalmonsters/rails-composer/master/files/app/assets/stylesheets/application-bootstrap.css.scss", "app/assets/stylesheets/application.css"
+copy_from_repo 'app/assets/stylesheets/application-bootstrap.css.scss',
+  :repo => 'https://raw.github.com/Taalmonsters/rails-composer/master/files/'
+copy_file 'app/assets/stylesheets/application-bootstrap.css.scss', 'app/assets/stylesheets/application.css.scss'
+copy_file 'app/assets/stylesheets/application.css.scss', 'app/assets/stylesheets/application.scss'
 
 git add: "."
 git commit: %Q{ -m 'Commit after generate' }
