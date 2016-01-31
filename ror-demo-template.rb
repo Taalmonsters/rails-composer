ca63b4cacf0712701017478f01933ac795af6573
matjemeisje <matje@taalmonsters.nl>
2016-01-31 17:34:50 +0100

    Clean up Taalmonsters Demo generator

--- ror-demo-template.rb
+++ ror-demo-template.rb
@@ -1,23 +1,4 @@
 apply "https://raw.github.com/Taalmonsters/rails-composer/master/devise-gmail-composer.rb"
 
-# route 'root :to => "sessions#new"'
-# route 'get "sign_up" => "users#new", :as => "sign_up"'
-# route 'get "log_in" => "sessions#new", :as => "log_in"'
-# route 'get "log_out" => "sessions#destroy", :as => "log_out"'
-# route 'get "workspace" => "users#workspace", :as => "workspace"'
-# route 'resources :users'
-# route 'resources :sessions'
-# 
-# generate(:model, 'user', 'name:string:uniq', 'email:string:uniq', 'password:digest')
-# generate(:controller, 'sessions', 'new', 'destroy')
-# generate(:controller, 'users', 'new', 'workspace')
-# 
-# gem 'bcrypt', '~> 3.1.7'
-# gem 'bootstrap-sass', '~> 3.3.1'
-# gem 'autoprefixer-rails'
-# 
-# after_bundle do
-  # git :init
-  # git add: "."
-  # git commit: %Q{ -m 'Initial commit' }
-# end
+git add: "."
+git commit: %Q{ -m 'Commit after generate' }
