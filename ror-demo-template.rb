b7ac7b39b08eb977ea6ff12a0bb205b8695ff58b
matjemeisje <matje@taalmonsters.nl>
2016-02-01 12:48:34 +0100

    Add locales to ror-demo-template.rb

--- ror-demo-template.rb
+++ ror-demo-template.rb
@@ -5,6 +5,15 @@ copy_from_repo 'app/assets/stylesheets/application.css.scss',
 
 copy_from_repo 'app/views/layouts/_navigation-taalmonsters.html.erb',
   {:dest => 'app/views/layouts/_navigation.html.erb', :repo => 'https://raw.github.com/Taalmonsters/rails-composer/master/files/'}
+  
+copy_from_repo '36a24f487b20c3f73b8ac6d20e749257cb8148b1/devise.nl.yml',
+  {:dest => 'config/locales/devise.nl.yml', :repo => 'https://gist.githubusercontent.com/jwkloos/f3136056b96241f1d46f/raw/'}
+  
+copy_from_repo '5c7b9fe5572e6783c47de7e0366ae54c16b5d0ab/devise_invitable.nl.yml',
+  {:dest => 'config/locales/devise_invitable.nl.yml', :repo => 'https://gist.githubusercontent.com/scambra/1311861/raw/'}
+
+copy_from_repo 'config/locales/nl.yml',
+  {:repo => 'https://raw.github.com/Taalmonsters/rails-composer/master/files/'}
 
 git add: "."
 git commit: %Q{ -m 'Commit after generate' }
